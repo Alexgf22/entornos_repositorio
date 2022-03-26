0d2717816e20d13105b00248d53f9a2b6a864681
Alex22 <agarfer2201@g.educaand.es>
2022-03-26 14:04:55 +0100

    test del ejercicio 61 añadido

---
+++ Ej61KtTest.kt
@@ -0,0 +1,12 @@
+import  org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class Ej61KtTest() {
+    @Test
+
+    fun testEj61() {
+        assert(coste(8,22.50) == 180.0)
+    }
+}
+
+
